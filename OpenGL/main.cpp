b4d71f6e3a8c5d1e6714e41c13e986f892513d05
CHRISTIAN LIMA SANTANA <christian.lsantana@senacsp.edu.br>
2025-08-27 20:48:17 -0300

    created an triangle

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,71 +1,110 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW//glfw3.h>
+#include <stdlib.h>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 const GLint WIDTH = 800, HEIGHT = 600;
-GLuint VAO,VBO,shaderProgram; 
-// VAO = espaço para alocação de itens
-// VBO = item/dado a ser alocado
+// VAO é o ponteiro do programa que estou rodando, espaço de memória que representa aquele objeto
+// VBO é o ponteiro para os meus dados brutos, são os dados do objeto a ser representado pelo VAO 
+// shaderProgram é qual programa estou rodando 
+// todo programa pode ser chamado de shader
+GLuint VAO, VBO, shaderProgram;
 
-static const char* vertexShader = "					\n\
-	#version 330									\n\
-													\n\
-	layout(location=0) in vec2 pos;					\n\
-													\n\
-	void main(){									\n\
-		gl_position = vec4(pos.x,pos.y, 0.0, 1.0)	\n\
-	}												\n\
-"; //posionamento de x e y na tela (forma de transferencia de dados: layout)
+// aqui estamos fazendo um programa (shader) em GLSL
 
-static const char* fragmentShader = "				\n\
-	#version 330									\n\
-													\n\
-	uniform in vec3 colorArr;						\n\
-													\n\
-	void main(){									\n\
-		color = vec4(colorArr, 1.0)					\n\
-	}												\n\
-"; //mudança de caracteristicas dado um ponto (forma de transferencia de dados: uniform)
+// shader para renderizar pontos na tela
+static const char* vertexShader = "                                                        \n\
+#version 330                                                                               \n\
+                                                                                           \n\
+                                                                                           \n\
+// passando um argumento para o inicio do programa (args do C//                            \n\
+// estou passando um argumento de entrada na primeira posiçâo                              \n\
+// esse argumento deve ser um vetor de duas posições                                       \n\
+layout(location=0) in vec2 pos;                                                            \n\
+                                                                                           \n\
+void main() {                                                                              \n\
+	gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);                                            \n\
+}                                                                                          \n\
+";
 
+// fragment pode ser lido como "a partir desse ponto o que faço com ele?"
+// shader para atribuir cores aos pontos
 
-void createTriangle() {
-	GLfloat vertices[] = {
-		0.0f, 1.0f, //v1
-		-1.0f, -1.0f, //v2
-		1.0f, -1.0f //v3
+static const char* fragmentShader = "                                                      \n\
+#version 330                                                                               \n\
+                                                                                           \n\
+// diferente da entrada por layout, uniform é uma entrada em tempo de execução             \n\
+uniform vec3 triColor;																       \n\
+out vec4 color;                                                                            \n\
+                                                                                           \n\
+void main() {                                                                              \n\
+	color = vec4(triColor, 1.0);                                                           \n\
+}                                                                                          \n\
+";
+
+void create_triangle() {
+	GLfloat vertices[] = { // nosso buffer de vertíces
+		0.0f, 1.0f, // vertice 1 
+		-1.0f, -1.0f, // vertice 2
+		1.0f, -1.0f // vertice 3
 	};
+
+	// iniciar um VAO
+	// vamos abrir um espaço de memória na placa de vídeo
+	// pegamos o endereço virutal da placa de video e guardamos ele na variável lá em cima
 	glGenVertexArrays(1, &VAO);
+
+	// estamos dizendo que a partir desse momento todas as alterações serão feitas nesse espaço de memória
+	// agora tudo o que vamos criar será nesse espaço de memória como os pontos da nossa tela
 	glBindVertexArray(VAO);
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0); //location
-	glBindBuffer(GL_ARRAY_BUFFER,0);
+
+
+	glGenBuffers(1, &VBO); // alocar um buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // a partir desse momento estamos modificando esse buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // explicando onde estão os dados
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0); // explicando como interpretar esses dados
+	glEnableVertexAttribArray(0);  // location
+
+	// deixar de apontar para o VAO e VBO
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
 
-void addTriangle(GLuint program, const char* shaderCode, GLenum type_program) {
-	GLuint _shader = glCreateShader(type_program);
-	const GLchar* glCharCode[1];
-	glCharCode[0] = shaderCode;
-	glShaderSource(_shader, 1, glCharCode,NULL);
+// função para executar um buffer na tela
+void add_shader(GLuint program, const char* shaderCode, GLenum type) {
+	GLuint _shader = glCreateShader(type);
+
+	// converte char para glchar
+	const GLchar* code[1];
+	code[0] = shaderCode;
+	glShaderSource(_shader, 1, code, NULL);
 	glCompileShader(_shader);
+
+
+	// tratar os erros
+
 	glAttachShader(program, _shader);
 }
 
-void addProgram() {
+void add_program() {
 	shaderProgram = glCreateProgram();
 	if (!shaderProgram) {
-		cout << "Erro ao criar o programa!";
-		return; 
+		cout << "Erro: falha ao criar o programa!";
 	}
-	addTriangle(shaderProgram, vertexShader, GL_VERTEX_SHADER);
-	addTriangle(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+
+	add_shader(shaderProgram, vertexShader, GL_VERTEX_SHADER);
+	add_shader(shaderProgram, fragmentShader, GL_FRAGMENT_SHADER);
+
 	glLinkProgram(shaderProgram);
 }
+
+
+
+
 
 int main() {
 	// inicando GLFW
@@ -96,7 +135,7 @@ int main() {
 	glfwMakeContextCurrent(window); // tornando essa janela como principal
 
 	//iniciando o Glew
-	glewExperimental = GLU_TRUE;
+	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK) {
 		cout << "Erro: não foi possível iniciar o glew";
 		glfwDestroyWindow(window);
@@ -106,10 +145,35 @@ int main() {
 
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
+	create_triangle();
+	add_program();
+	srand(time(NULL));
+
+
+
+	float randomColors[3];
 	while (!glfwWindowShouldClose(window)) {
-		glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+		//Cor de fundo da janela
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Altera cor do triangulo
+		GLint uniformColor = glGetUniformLocation(shaderProgram, "triColor");
+		randomColors[0] = (rand() % 100);
+		randomColors[1] = (rand() % 100);
+		randomColors[2] = (rand() % 100);
+		printf("%f", randomColors[0]);
+		glUniform3f(uniformColor, randomColors[0], randomColors[1], randomColors[2]);
+
+		//Desenhando o triangulo
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3); //Tringulo, começando na posição 0, Numero de pontos 3
+		glBindVertexArray(0);
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
 		glfwSwapBuffers(window);
 	}
 
